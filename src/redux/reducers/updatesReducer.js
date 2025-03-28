33d5594914aa1da304dc270bfde17b1a2ddeb3fe
Fode Sama <magassbangalyfode@gmail.com>
2025-03-28 15:55:37 +0100

    Display updated

--- src/redux/reducers/updatesReducer.js
+++ src/redux/reducers/updatesReducer.js
@@ -1,15 +1,25 @@
+import photo2 from "../../images/photo 2.jpg";
+import photo3 from "../../images/photo 3.jpeg";
+import photo4 from "../../images/photo 4.jpg";
+
 const initialState = [
   {
     name: "John Doe",
     message: "added a new product",
     time: "2 minutes ago",
-    photo: "./images/profile1.jpg",
+    photo: photo2,
   },
   {
     name: "Jane Smith",
     message: "updated the dashboard",
     time: "10 minutes ago",
-    photo: "./images/profile2.jpg",
+    photo: photo3,
+  },
+  {
+    name: "Jane Smith",
+    message: "updated the dashboard",
+    time: "10 minutes ago",
+    photo: photo4,
   },
 ];
 
